e3d79c075a31001f375488aa93aa0cfa3f0e3e87
JarlBentzen <113165737+JarlBentzen@users.noreply.github.com>
2025-05-14 13:49:34 +0200

    walking anim done

--- guack'em all/Assets/Scripts/Player.cs
+++ guack'em all/Assets/Scripts/Player.cs
@@ -2,53 +2,55 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   
     public float moveSpeed = 5f;
 
     private Rigidbody2D myRigidbody;
     private Vector2 moveDirection = Vector2.zero;
     private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
-    // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-  void Update()
-{
-    moveDirection = Vector2.zero;
-
-    if (Input.GetKey(KeyCode.A)) moveDirection = Vector2.left;
-    if (Input.GetKey(KeyCode.D)) moveDirection = Vector2.right;
-    if (Input.GetKey(KeyCode.W)) moveDirection = Vector2.up;
-    if (Input.GetKey(KeyCode.S)) moveDirection = Vector2.down;
-
-    FlipSprite();
-}
- void FixedUpdate()
-{
-    myRigidbody.linearVelocity = moveDirection * moveSpeed;
-}
-    void FlipSprite()
+    void Update()
     {
-        if (moveDirection == Vector2.left)
+        // Get input for both axes to allow diagonal movement
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if (Input.GetKey(KeyCode.A)) moveX = -1f;
+        if (Input.GetKey(KeyCode.D)) moveX = 1f;
+        if (Input.GetKey(KeyCode.W)) moveY = 1f;
+        if (Input.GetKey(KeyCode.S)) moveY = -1f;
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+
+        FlipSprite(moveDirection);
+        
+        // Send speed to Animator
+        animator.SetFloat("Speed", moveDirection.magnitude);
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.linearVelocity = moveDirection * moveSpeed;
+    }
+
+    void FlipSprite(Vector2 dir)
+    {
+        // Only flip left/right based on x movement
+        if (dir.x < 0)
         {
-            transform.rotation = Quaternion.Euler(0, 180, 0); // Face Left
+            transform.rotation = Quaternion.Euler(0, 180, 0); // Face left
         }
-        else if (moveDirection == Vector2.right)
+        else if (dir.x > 0)
         {
-            transform.rotation = Quaternion.Euler(0, 0, 0); // Face Right
+            transform.rotation = Quaternion.Euler(0, 0, 0); // Face right
         }
-        else if (moveDirection == Vector2.up)
-        {
-            transform.rotation = Quaternion.Euler(0, 0, 90); // Face Up
-        }
-        else if (moveDirection == Vector2.down)
-        {
-            transform.rotation = Quaternion.Euler(0, 0, -90); // Face Down
-        }
+        // Do nothing if only moving vertically
     }
 }
