ef1c11a4aa4d6cedaa9ab09b0bc624ce09a2c8ce
Maciej Rutkowski <eaamrut@students.eaaa.dk>
2025-05-16 09:11:11 +0200

    First basic shop UI

--- guack'em all/Assets/Scripts/Shop.cs
+++ guack'em all/Assets/Scripts/Shop.cs
@@ -1,32 +1,74 @@
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class Shop : MonoBehaviour
 {
-    
-        public int Coin;
-        public int Avocado;
-        public Text Coin_text;
-        public Text Avocado_text;
+    public int Coin;
+    public int Maracas;
+    public int Cactus;
+    public int Chicken;
+
+    public TextMeshProUGUI Coin_text;
+    public TextMeshProUGUI Maracas_text;
+    public TextMeshProUGUI Cactus_text;
+    public TextMeshProUGUI Chicken_text;
+
+       public TextMeshProUGUI BuyChickenButtonText;
+    public TextMeshProUGUI BuyCactusButtonText;
+    public TextMeshProUGUI BuyMaracasButtonText;
+
     void Start()
     {
-            Coin = 2000;
-            Coin_text.text = Coin.ToString();
+        Coin = 2000;
+        Coin_text.text = Coin.ToString();
     }
 
- public void BuyAvocado() 
- {
-        if(Coin >= 200)
+    public void BuyChicken()
+    {
+        if (Coin >= 500)
+        {
+            Coin -= 500;
+            Coin_text.text = Coin.ToString();
+
+            Chicken += 1;
+            Chicken_text.text = "Equipped!";
+            BuyChickenButtonText.text = "Purchased!";
+        }
+        else
+        {
+            print("Not enough coins!");
+        }
+    }
+
+    public void BuyCactus()
+    {
+        if (Coin >= 350)
+        {
+            Coin -= 350;
+            Coin_text.text = Coin.ToString();
+
+            Cactus += 1;
+            Cactus_text.text = "Purchased!";
+        }
+        else
+        {
+            print("Not enough coins!");
+        }
+    }
+
+    public void BuyMaracas()
+    {
+        if (Coin >= 200)
         {
             Coin -= 200;
             Coin_text.text = Coin.ToString();
 
-            Avocado += 1;
-            Avocado_text.text = Avocado.ToString();
+            Maracas += 1;
+            Maracas_text.text = "Purchased!";
         }
-        else 
+        else
         {
             print("Not enough coins!");
         }
- }
+    }
 }
