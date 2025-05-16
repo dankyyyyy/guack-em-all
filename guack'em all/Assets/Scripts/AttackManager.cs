a932c4a8140420ad80f1a9810063081048ffbf96
Aleksandar-Mihail <93515589+dankyyyyy@users.noreply.github.com>
2025-05-16 13:23:28 +0200

    Finally working ScriptableObjects?
    - Reverted some of the changes in the previous commit
    - Renamed WeaponData to WeaponSO for clarity
    - Moved audio files into the audio folder
    - Modified AttackManager.cs to make use of SOs
    
    ------------------
    TODO Fix trails and particles - they got broken during a merge and we didn't notice.

--- guack'em all/Assets/Scripts/AttackManager.cs
+++ guack'em all/Assets/Scripts/AttackManager.cs
@@ -3,145 +3,119 @@ using UnityEngine;
 
 public class AttackManager : MonoBehaviour
 {
-    private Animator weaponAnimator;
-    private TrailRenderer currentTrailRenderer;
-    private ParticleSystem currentSwipeParticles;
     public bool IsPlayerSwinging { get; private set; } = false;
-    [SerializeField] private Transform weaponTransform; // The attachment point, not necessarily the weapon itself anymore
-    [SerializeField] private WeaponData currentWeaponData; // Assign the current weapon's data in the Inspector (can be managed by WeaponEquipper)
-    [HideInInspector] public int damagedHealth;
-    private AudioSource audioSource;
+    [SerializeField] private WeaponSO currentWeapon; // Assign the current weapon's data in the Inspector
+    [SerializeField] private Transform weaponTransform;
+    [SerializeField] private Animator weaponAnimator;
+    [SerializeField] private AudioSource weaponAudioSource;
+    [SerializeField] private AudioClip[] attackSounds;
+    //[SerializeField] private TrailRenderer weaponTrail;
+    [SerializeField] private SpriteRenderer weaponSpriteRenderer;
 
-    private WeaponEquipper weaponEquipper; // Reference to the WeaponEquipper script
-    
+
+    public int damagedHealth;
 
     void Start()
     {
-        weaponEquipper = FindFirstObjectByType<WeaponEquipper>(); // Assuming WeaponEquipper is on the player or a parent
-
-        if (weaponEquipper == null)
-        {
-            Debug.LogError("WeaponEquipper not found on the player or parent!");
-        }
-
-        audioSource = GetComponent<AudioSource>();
-        if (audioSource == null)
-        {
-            audioSource = gameObject.AddComponent<AudioSource>();
-        }
-        audioSource.volume = 0.3f;
-
-        // Initialize based on the currently equipped weapon (if any at start)
-        UpdateWeaponComponents();
-    }
-
-    // Call this method whenever the equipped weapon changes
-    public void UpdateWeaponComponents()
-    {
-        if (weaponEquipper != null && weaponEquipper.currentWeaponInstance != null)
-        {
-            weaponAnimator = weaponEquipper.currentWeaponInstance.GetComponent<Animator>();
-            currentTrailRenderer = weaponEquipper.currentWeaponInstance.GetComponent<TrailRenderer>();
-            currentSwipeParticles = weaponEquipper.currentWeaponInstance.GetComponent<ParticleSystem>(); // Assuming particles are on the weapon prefab too
-
-            if (weaponAnimator == null)
-            {
-                Debug.LogWarning("Animator not found on the equipped weapon!");
-            }
-            if (currentTrailRenderer != null)
-            {
-                currentTrailRenderer.emitting = false;
-            }
-            if (currentSwipeParticles != null)
-            {
-                currentSwipeParticles.Stop();
-            }
-            if (currentWeaponData != null && currentWeaponData.weaponAnimatorOverride != null && weaponAnimator != null)
-            {
-                weaponAnimator.runtimeAnimatorController = currentWeaponData.weaponAnimatorOverride;
-            }
-        }
-        else
-        {
-            weaponAnimator = null;
-            currentTrailRenderer = null;
-            currentSwipeParticles = null;
-        }
+        weaponTransform = GetComponentInChildren<Transform>();
+        weaponAnimator = GetComponentInChildren<Animator>();
+        weaponAudioSource = GetComponentInChildren<AudioSource>();
+        attackSounds = currentWeapon.attackSounds;
+        //weaponTrail = currentWeapon.weaponTrail;
+        weaponSpriteRenderer.sprite = currentWeapon.weaponSprite;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && currentWeapon != null && IsPlayerSwinging == false)
         {
             StartSwing();
-
-            // Trigger the animation on the weapon's animator
-            if (weaponAnimator != null)
-            {
-                weaponAnimator.SetTrigger("Attack");
-            }
-
-            // Enable trail
-            if (currentTrailRenderer != null)
-            {
-                currentTrailRenderer.emitting = true;
-            }
-
-            // Play particles
-            if (currentSwipeParticles != null)
-            {
-                currentSwipeParticles.Play();
-            }
-
-            PlayAttackSound();
-
-            // Disable trail shortly after
-            Invoke("StopTrail", currentWeaponData.swingLength);
-        }
-    }
-
-    void StopTrail()
-    {
-        if (currentTrailRenderer != null)
-        {
-            currentTrailRenderer.emitting = false;
         }
     }
 
     public void StartSwing()
     {
         IsPlayerSwinging = true;
-        StartCoroutine(EndSwingAfterDelay(currentWeaponData.swingLength));
+
+        // // Enable trail emission
+        // if (currentTrailRenderer != null)
+        // {
+        //     currentTrailRenderer.Clear(); // optional: avoids trail residue
+        //     currentTrailRenderer.emitting = true;
+        // }
+
+        // // Play particles
+        // if (currentSwipeParticles != null)
+        // {
+        //     currentSwipeParticles.Play();
+        // }
+
+        PlayAttackSound();
+
+        // Trigger the animation on the weapon's animator
+        if (weaponAnimator != null)
+        {
+            weaponAnimator.SetTrigger("Attack");
+        }
+
+        // Disable trail shortly after
+        Invoke("StopTrail", currentWeapon.swingLength); // Use the swingLength from the current weapon data
+
+        StartCoroutine(EndSwingAfterDelay(currentWeapon.swingLength));
     }
+
+    public void EndSwing()
+    {
+        IsPlayerSwinging = false;
+
+        // Disable trail emission
+        // if (currentTrailRenderer != null)
+        // {
+        //     currentTrailRenderer.emitting = false;
+        // }
+
+        // // Stop particles
+        // if (currentSwipeParticles != null)
+        // {
+        //     currentSwipeParticles.Stop();
+        // }
+    }
+
     IEnumerator EndSwingAfterDelay(float delay)
     {
         yield return new WaitForSeconds(delay);
         EndSwing();
     }
-    public void EndSwing()
-    {
-        IsPlayerSwinging = false;
-    }
+
+    // private void ApplyWeaponVisuals()
+    // {
+    //     if (currentWeapon == null) return;
+
+    //     if (weaponSpriteRenderer != null && currentWeapon.weaponSprite != null)
+    //     {
+    //         weaponSpriteRenderer.sprite = currentWeapon.weaponSprite;
+    //     }
+    // }
 
     public void DealDamage(int targetHealth)
     {
-        if (currentWeaponData != null)
+        if (currentWeapon != null)
         {
-            damagedHealth = targetHealth - currentWeaponData.damage;
+            damagedHealth = targetHealth - currentWeapon.damage; // Use damage from weapon data
         }
         else
         {
             Debug.LogError("No Weapon Data assigned to the Attack Manager!");
-            damagedHealth = targetHealth;
+            damagedHealth = targetHealth; // Avoid potential errors
         }
     }
 
     void PlayAttackSound()
     {
-        if (currentWeaponData != null && currentWeaponData.attackSounds.Length > 0)
+        if (attackSounds.Length > 0)
         {
-            AudioClip clip = currentWeaponData.attackSounds[Random.Range(0, currentWeaponData.attackSounds.Length)];
-            audioSource.PlayOneShot(clip);
+            AudioClip clip = attackSounds[Random.Range(0, attackSounds.Length)];
+            weaponAudioSource.PlayOneShot(clip);
         }
     }
 }
--- guack'em all/Assets/Scripts/WeaponSO.cs
+++ guack'em all/Assets/Scripts/WeaponSO.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "WeaponData", menuName = "Scriptable Objects/WeaponData")]
-public class WeaponData : ScriptableObject
+[CreateAssetMenu(fileName = "WeaponSO", menuName = "Scriptable Objects/WeaponSO")]
+public class WeaponSO : ScriptableObject
 {
     [Header("Weapon Information")]
     public string weaponName = "New Weapon";
@@ -11,13 +11,8 @@ public class WeaponData : ScriptableObject
 
     [Header("Visuals, Sound & Animation")]
     public AudioClip[] attackSounds;
-    public GameObject trailPrefab; // Optional: Prefab for the trail effect
-    public GameObject swipeParticlesPrefab; // Optional: Prefab for swipe particles
     public Sprite weaponSprite; // Sprite for the weapon
-    public RuntimeAnimatorController weaponAnimatorController; // Animator Controller for the weapon
-    public AnimatorOverrideController weaponAnimatorOverride; // Optional override
 
     [Header("Weapon Setup")]
     public GameObject weaponPrefab; // Entire prefab containing SpriteRenderer, Animator, Collider, etc.
-
 }
