384cf985b4b371d97afe0386788b2d3913d8d951
Aleksandar-Mihail <93515589+dankyyyyy@users.noreply.github.com>
2025-05-20 13:25:14 +0200

    HOTFIX Removed a declaration

--- guack'em all/Assets/Scripts/GameManager.cs
+++ guack'em all/Assets/Scripts/GameManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using static SceneLoader;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
